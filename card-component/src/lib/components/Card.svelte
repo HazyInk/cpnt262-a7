b62fd59505646048fb19efedfc3758fcd47dcb25
HazyInk <reillysheapaston@gmail.com>
2023-11-08 08:53:58 -0700

    added all files

---
+++ card-component/src/lib/components/Card.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Card from "components/Card.svelte";
+
+</script>
+  <a href="https://hazyink.github.io/cpnt260-a2/">
+    <img class="rounded-xl h-25" src="static/favicon.png" alt="">
+    <h1 class="text-purple-800 font-serif text-3xl text-center pt-5 pb-5">Digital Art Work</h1>
+  <h3 class="text-purple-800 font-mono text-xl text-center pt-2 pb-2">Sale: $20 CAD</h3>
+    <p class="text-purple-800 font-mono text-sm text-center pt-2 pb-2">"A digital piece that can be bought and printed off on a canvas, poster, card, phone case ect..."</p>
+  <h6 class="text-purple-800 font-mono text-xs text-center pt-2 pb-3">Click the card to find out more</h6>
+  </a>
